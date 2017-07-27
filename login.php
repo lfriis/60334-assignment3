240059151c2ecca4870f4ae34669e8cb3373f83d
Larsen Friis <larsenfriis@icloud.com>
2017-07-27 10:15:36 -0400

    Add files via upload

---
+++ login.php
@@ -0,0 +1,10 @@
+<?php //login.php
+
+     $hn ='localhost'; //hostname
+     $un ='friis_pbl'; //username
+     $pw ='**Insert Password**'; //password
+     $db ='friis_pbl'; //database
+
+$conn = new mysqli($hn, $un, $pw, $db);
+
+?>
